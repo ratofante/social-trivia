003748c25c1c4b6a2489fcc25da967d00da83a1a
Rodrigo González Wilkens <libra.rgw@gmail.com>
2024-02-28 15:31:08 -0300

    +Accordion;

---
+++ resources/js/Components/Accordion/AccordionBlock.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        class="grid accordion-transition my-4"
+        :class="[isOpen ? 'accordion-open' : 'accordion-closed']"
+    >
+        <button class="flex justify-between text-sm" @click="isOpen = !isOpen">
+            <span>
+                <slot name="trigger" />
+            </span>
+            <ChevronDownIcon
+                class="w-6 h-6 transition-transform text-green opacity-80"
+                :class="isOpen ? 'rotate-180' : 'rotate-0'"
+            />
+        </button>
+        <div class="overflow-hidden">
+            <slot name="content" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { ChevronDownIcon } from "@heroicons/vue/20/solid";
+
+const isOpen = ref(false);
+</script>
