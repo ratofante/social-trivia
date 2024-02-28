60cecb46905831528e0f2cac6fdf8082681b201c
Rodrigo González Wilkens <libra.rgw@gmail.com>
2024-02-28 15:31:31 -0300

    +QuestionIcon; +QuestionScore;

---
+++ resources/js/Components/CategoryIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <BeakerIcon v-if="category === 4" :class="classes" />
+    <PaintBrushIcon v-if="category === 1" :class="classes" />
+    <TrophyIcon v-if="category === 2" :class="classes" />
+    <GlobeAmericasIcon v-if="category === 3" :class="classes" />
+</template>
+
+<script setup>
+import {
+    BeakerIcon,
+    PaintBrushIcon,
+    TrophyIcon,
+    GlobeAmericasIcon,
+} from "@heroicons/vue/20/solid";
+
+defineProps({
+    category: {
+        required: true,
+    },
+    classes: {
+        default: "",
+    },
+});
+</script>
---
+++ resources/js/Components/Question/QuestionScore.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex gap-2 text-end text-sm text-gray-light tracking-wide">
+        Puntaje:
+        <span
+            class="font-medium tracking-tight"
+            :class="{
+                'text-green': score >= 75,
+                'text-blue-500': score >= 50 && score < 75,
+                'text-orange-500': score >= 25 && score < 50,
+                'text-red-500': score < 25,
+            }"
+            >{{ score }}/100</span
+        >
+    </div>
+</template>
+
+<script setup>
+defineProps(["score"]);
+</script>
